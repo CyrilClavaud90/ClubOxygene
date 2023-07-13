fec61b545910c3080f6b7f9678af393980c42035
Cyril Clavaud <c.clavaud90@gmail.com>
2023-07-13 21:59:48 +0200

    Mise en place du footer et de son css

--- ClubOxygene/src/components/App/App.jsx
+++ ClubOxygene/src/components/App/App.jsx
@@ -1,9 +1,12 @@
+import Footer from "../Footer"
 import Header from "../Header/Index"
 
 function App() {
   return (
     <div className="body__container">
       <Header />
+
+      <Footer />
     </div>
   )
 }
